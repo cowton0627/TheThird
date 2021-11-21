ffd5af80ab6491128cad3b05b2400f63d6fd0d92
鄭淳澧 <cowton0517@gmail.com>
2021-11-21 14:47:22 +0800

    [Modify] 關於轉場

--- TheThird/ViewController.swift
+++ TheThird/ViewController.swift
@@ -9,11 +9,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    required init?(coder: NSCoder) {
+        print("===> ViewController init")
+        super.init(coder: coder)
+    }
+    
+    deinit {
+        print("ViewController deinit <===")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
-
+    @IBAction func unwindToVC(for unwindSegue: UIStoryboardSegue) {}
+    
+    @IBAction func toTheSecond(_ sender: Any) {
+//        let sbd = UIStoryboard(name: "Main", bundle: nil)
+//        let vc = sbd.instantiateViewController(withIdentifier: "\(SecondViewController.self)")
+//        navigationController?.pushViewController(vc, animated: true)
+//        show(vc, sender: nil)
+        
+//        present(vc, animated: true) {
+//            print("Hello World!")
+//        }
+        
+//        ChangeScene.to(selfVC: self, targetSbdName: "Main", targetVCname: "\(SecondViewController.self)")
+        ChangeScene.to(selfVC: self, targetSbdName: "Sub", targetVCname: "\(ThirdViewController.self)")
+//        ChangeScene.to(selfVC: self, targetSbdName: "Sub")
+        
+    }
+    
 }
 
